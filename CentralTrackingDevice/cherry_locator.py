a60584ecde3fefda955e841374535e7d5a8b89b6
artemki2077 <artem.kiselev.2005mail.ru@gmail.com>
2023-05-02 18:07:03 +0300

    test cherry 5

--- CentralTrackingDevice/cherry_locator.py
+++ CentralTrackingDevice/cherry_locator.py
@@ -7,31 +7,37 @@ img = cv2.imread("img.png")
 # (396, 364) (472, 488)
 # img_2 = img[382:494, 1176:1254]
 # img_2 = img[364:488, 396:472]
-img_2 = img[766:864, 115:166]
+cords = [((412, 376), (466, 474)), 
+        ((828, 133), (856, 183)), 
+        ((1188, 383), (1247, 487))]
 
-hsv = cv2.cvtColor(img_2, cv2.COLOR_BGR2HSV )
-h1, s1, v1, h2, s2, v2 = 16, 0, 0, 255, 255, 255
+cord = cords[0]
+box_img = frame[cord[0][1]:cord[1][1], cord[0][0]:cord[1][0]]
 
-h_min = np.array((h1, s1, v1), np.uint8)
-h_max = np.array((h2, s2, v2), np.uint8)
-thresh = cv2.inRange(hsv, h_min, h_max)
 
-kernel = np.ones((1, 1), 'uint8')
-thresh = cv2.erode(thresh, kernel, iterations=5)
-thresh = cv2.dilate(thresh, kernel, iterations=5)
+# hsv = cv2.cvtColor(img_2, cv2.COLOR_BGR2HSV )
+# h1, s1, v1, h2, s2, v2 = 16, 0, 0, 255, 255, 255
 
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# h_min = np.array((h1, s1, v1), np.uint8)
+# h_max = np.array((h2, s2, v2), np.uint8)
+# thresh = cv2.inRange(hsv, h_min, h_max)
 
-res = False
-for contour in contours:
-    area = cv2.contourArea(contour)
-    # print(area)
-    if area > 5000:
-        res = True
-        break
-        # print("YES")
-        # exit()
+# kernel = np.ones((1, 1), 'uint8')
+# thresh = cv2.erode(thresh, kernel, iterations=5)
+# thresh = cv2.dilate(thresh, kernel, iterations=5)
+
+# contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+# res = False
+# for contour in contours:
+#     area = cv2.contourArea(contour)
+#     # print(area)
+#     if area > 5000:
+#         res = True
+#         break
+#         # print("YES")
+#         # exit()
         
-cv2.imshow('image', thresh)
+cv2.imshow('image', box_img)
 cv2.waitKey(0)
 
