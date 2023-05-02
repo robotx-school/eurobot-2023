463f2725dd4d2e6187689c041056773baff0b3a6
artemki2077 <artem.kiselev.2005mail.ru@gmail.com>
2023-05-02 17:46:58 +0300

    test cherry 4

--- CentralTrackingDevice/ctd_server.py
+++ CentralTrackingDevice/ctd_server.py
@@ -156,30 +156,28 @@ class FindСherry:
             res_cord = False
             ret, frame = self.localizator.camera.read()
             box_img = frame[cord[0][1]:cord[1][1], cord[0][0]:cord[1][0]]
+            if box_img:
+                cv2.imshow('test', box_img)
+                cv2.waitKey(0)
+                hsv = cv2.cvtColor(box_img, cv2.COLOR_BGR2HSV )
+                h1, s1, v1, h2, s2, v2 = 16, 0, 0, 255, 255, 255
 
-            print(box_img)
-            print('______')
-            # cv2.imshow('test', box_img)
-            # cv2.waitKey(0)
-            # hsv = cv2.cvtColor(box_img, cv2.COLOR_BGR2HSV )
-            # h1, s1, v1, h2, s2, v2 = 16, 0, 0, 255, 255, 255
+                h_min = np.array((h1, s1, v1), np.uint8)
+                h_max = np.array((h2, s2, v2), np.uint8)
+                thresh = cv2.inRange(hsv, h_min, h_max)
 
-            # h_min = np.array((h1, s1, v1), np.uint8)
-            # h_max = np.array((h2, s2, v2), np.uint8)
-            # thresh = cv2.inRange(hsv, h_min, h_max)
+                kernel = np.ones((1, 1), 'uint8')
+                thresh = cv2.erode(thresh, kernel, iterations=5)
+                thresh = cv2.dilate(thresh, kernel, iterations=5)
 
-            # kernel = np.ones((1, 1), 'uint8')
-            # thresh = cv2.erode(thresh, kernel, iterations=5)
-            # thresh = cv2.dilate(thresh, kernel, iterations=5)
+                contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+                for contour in contours:
+                    area = cv2.contourArea(contour)
+                    if area > 5000:
+                        res_cord = True
+                        break
 
-            # contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-            # for contour in contours:
-            #     area = cv2.contourArea(contour)
-            #     if area > 5000:
-            #         res_cord = True
-            #         break
-
-            # res.append(res_cord)
+            res.append(res_cord)
         return res
             
                     
