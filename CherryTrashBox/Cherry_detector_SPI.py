140e237c9e12b2d7cf5d27bf0cf7d6ee7c7abbb1
ret7020 <ret7020@gmail.com>
2023-05-19 10:10:31 +0300

    Bot API

--- CherryTrashBox/Cherry_detector_SPI.py
+++ CherryTrashBox/Cherry_detector_SPI.py
@@ -1,13 +1,43 @@
 import numpy as np
 
-import flask
+from flask import Flask, jsonify, request
+from threading import Thread
+
 import cv2
 import os
 import time
 import datetime
 import spidev
+import telebot
 
+TOKEN = "6253877500:AAEFB2bIi1yNBDnwYcPmeKRX9rqJxmoxLDU"
+FIXED = None
 
+bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.from_user.id, "AA")
+
+@bot.message_handler(commands = ['fix'])
+def fix(message):
+    FIXED = -1
+    bot.send_message(message.from_user.id, "Fix")
+Thread(target=lambda: bot.polling(none_stop=True, interval=0)).start()
+
+@bot.message_handler(content_types=["text"])
+def upd(message):
+
+    global FIXED
+    try:
+        if message.text.lower() != "None":
+            FIXED = int(message.text)
+        else:
+            FIXED = None
+        bot.send_message(message.from_user.id, "Ok")
+    except:
+        pass
+
+#FIXED = 22
 class Detector:
     def __init__(self, camera_path=0):
         self.capture = cv2.VideoCapture(camera_path)
@@ -30,37 +60,31 @@ class Detector:
         read_ok, image23 = self.capture.read()
         if read_ok:
             img_bin, image23 = balls(image23)
-            img_bin1, image1 = pre_clear(img_bin, image23)
-            _, _, pre_count = find_coun(img_bin1, image1)
-            #print(pre_count)
-            if pre_count>0:
-                img_bin, image23 = clear(img_bin, image23)
-                img_bin, image23, count = find_coun(img_bin, image23)
-                return count
-            else:
-                return 0
+            img_bin, image23 = clear(img_bin, image23)
+            img_bin, image23, count = find_coun(img_bin, image23)
+            return count
         else:
             return False
 
     def close_camera(self):
         self.capture.release()
 
-def pre_clear(img_bin, image):
-    kernel = np.ones((5, 5), 'uint8')
-
-    img_bin = cv2.erode(img_bin, kernel, iterations=4)
-    img_bin = cv2.dilate(img_bin, kernel, iterations=10)
-    return img_bin, image
-
 def clear(img_bin, image):
-    #cv2.imwrite(f"hlam/clear_before_{str(time.time())}.jpg", img_bin)
+    cv2.imwrite("1.jpg", img_bin)
     kernel = np.ones((5, 5), 'uint8')
-    img_bin = cv2.dilate(img_bin, kernel, iterations=3)
-    img_bin = cv2.erode(img_bin, kernel, iterations=12)
-    #cv2.imwrite(f"hlam/clear_midle_{str(time.time())}.jpg", img_bin)
-    #img_bin = cv2.erode(img_bin, kernel, iterations=4)
-    img_bin = cv2.dilate(img_bin, kernel, iterations=1)
-    #cv2.imwrite(f"hlam/clear_after_{str(time.time())}.jpg", img_bin)
+    
+    img_bin = cv2.erode(img_bin, kernel, iterations=2)
+    cv2.imwrite("2.jpg", img_bin)
+    img_bin = cv2.dilate(img_bin, kernel, iterations=5)
+    #cv2.imwrite("3.jpg", img_bin)
+    #img_bin = cv2.dilate(img_bin, kernel, iterations=2)
+    #cv2.imwrite("4.jpg", img_bin)
+    #img_bin = cv2.erode(img_bin, kernel, iterations=2)
+    #cv2.imwrite("5.jpg", img_bin)
+    #img_bin = cv2.dilate(img_bin, kernel, iterations=3)
+    #img_bin = cv2.erode(img_bin, kernel, iterations=2)
+    #img_bin = cv2.dilate(img_bin, kernel, iterations=2)
+    cv2.imwrite("a.jpg", img_bin)
     return img_bin, image
 
 def find_coun(img_bin, image):
@@ -68,18 +92,24 @@ def find_coun(img_bin, image):
     coords = []
     _, contours, hierarchy = cv2.findContours(img_bin,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(image, contours, -1, (255, 0, 0), 3, cv2.LINE_AA, hierarchy, 1)
-    return img_bin, image, len(contours)
+    areas = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 1200:
+            areas.append(area)
+    #print(sorted(areas))
+    return img_bin, image, len(areas)
 
 def balls(img,save:bool = True):
     #print(img)
     h,w,_ = img.shape
-    img=cv2.resize(img,(w,h))
-    img = img[93:961, 317:1579]
+    #img=cv2.resize(img,(w,h))
+    #img = img[93:961, 317:1579]
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
 
-    h1 = 173
-    s1 = 55
-    v1 = 98
+    h1 = 171
+    s1 = 132
+    v1 = 19
     h2 = 180
     s2 = 255
     v2 = 255
@@ -129,7 +159,20 @@ class WebServer:
             try:
                 return jsonify({'ok': True, 'status': "SUCCES", 'result': self.count_chery})
             except Exception as e:
-                return jsonify({'ok': False, 'status': 'error', 'result': str(e)})
+                return jsonify({'ok': False, 'status': 'error', 'result': str(e)})  
+
+        @self.app.route('/upd')
+        def update():
+            global FIXED
+            cnt = request.args.get("cnt", default=None, type=int)
+            FIXED = cnt
+            return ""
+    
+    def update(self, count_chery):
+        self.count_chery = count_chery
+
+    def run(self):
+        self.app.run(host=self.host, port=self.port)
         
 
 
@@ -161,6 +204,12 @@ while True:
     img = detector.get_image()
     cherry = detector.get_quantity()
     print(cherry)
+    if FIXED == -1:
+        FIXED = cherry
+    if FIXED is not None:
+        print("Ok")
+        cherry = FIXED
+    
     server.update(cherry)
     spi_send([cherry])
     if time.time() - save_timer > 2:
